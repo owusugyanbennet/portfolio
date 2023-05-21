9cfcdec3a0f30dc07ee4a9070b9a59c612d6f58f
owusugyanbennet <126066201+owusugyanbennet@users.noreply.github.com>
2023-05-21 11:30:58 +0000

    My Portfolio Update

--- My_Portfolio/js/script.js
+++ My_Portfolio/js/script.js
@@ -3,7 +3,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle ('-bx-x');
+    menuIcon.classList.toggle ('bx-x');
     navbar.classList.toggle ('active');
 };
 
@@ -32,4 +32,35 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100); 
 
+    /*================= remove toggle icon and navbar when click navbar link (scroll) ==============*/
+    menuIcon.classList.remove ('bx-x');
+    navbar.classList.remove ('active');
+
+
 };
+
+    /*=================scroll reveal ==============*/
+    ScrollReveal({ 
+        //reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 
+        'bottom' });
+        ScrollReveal().reveal('.home-content h1, .about-img', { origin: 
+            'left' });
+            ScrollReveal().reveal('.home-content p, .about-content', { origin:  'right' });
+    
+
+    /*=================typed js ==============*/
+    const typed = new Typed (' .multiple-text', {
+        strings: ['Frontend Developer', 'Blogger', 'YouTuber'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+    
+ 
